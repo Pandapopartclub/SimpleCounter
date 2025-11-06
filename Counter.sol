077a734c40c575cc0eaaf66a1277ee54b78c15a0
Pandapopartclub <157401369+Pandapopartclub@users.noreply.github.com>
2025-11-06 15:50:58 +0100

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -1,12 +1,17 @@
-// SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
 contract Counter {
   uint32 private _count;
 
-  function getCount() external view returns (uint32) { return _count; }
-  function increment(uint32 value) external { _count += value; }
+  function getCount() external view returns (uint32) {
+    return _count;
+  }
+
+  function increment(uint32 value) external {
+    _count += value;
+  }
+
   function decrement(uint32 value) external {
     require(_count >= value, "Counter: cannot decrement below zero");
     _count -= value;
